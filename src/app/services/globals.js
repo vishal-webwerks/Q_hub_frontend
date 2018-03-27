9480a8a1eda90e162a06b376a0f57d0b3404385e
Vishal Raut <vishal.raut@wwindia.com>
2018-03-27 12:54:20 +0530

    removed node modules

--- src/app/services/globals.js
+++ src/app/services/globals.js
@@ -1,5 +1,5 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.appName = '<span>Neo</span><strong>SOFT</strong>';
-exports.apiLink = 'http://localhost:3009/';
+exports.apiLink = 'http://localhost:5000/';
 //# sourceMappingURL=globals.js.map
