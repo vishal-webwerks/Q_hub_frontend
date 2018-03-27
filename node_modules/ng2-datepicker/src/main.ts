e05f351f2b86acb669fc3eaf9ba0a1f01d4c22da
Vishal Raut <vishal.raut@wwindia.com>
2018-03-27 12:52:54 +0530

    Removed node modules

--- node_modules/ng2-datepicker/src/main.ts
+++
@@ -1,6 +0,0 @@
-import './styles';
-import './polyfills';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
